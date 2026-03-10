45fafb476b24ff3ae4255185da28ad57d875fbef
hareesh-27 <hareesh27d@gmail.com>
2026-03-10 17:44:04 +0530

    Create code.cpp

---
+++ DSA/Hashing/Longest subarray with sum k/code.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int longestSubarray(vector<int> &nums, int k) {
+        int n = nums.size();
+        int left = 0, right = 0;
+        int sum = nums[0];
+        int maxLen = 0;
+
+        while (right < n) {
+
+            while (left <= right && sum > k) {
+                sum -= nums[left];
+                left++;
+            }
+
+            if (sum == k) {
+                maxLen = max(maxLen, right - left + 1);
+            }
+
+            right++;
+            if (right < n)
+                sum += nums[right];
+        }
+
+        return maxLen;
+    }
+};
