b696e484488b9b2a8e30284df716a7f3b67dd05b
hareesh-27 <hareesh27d@gmail.com>
2026-03-24 19:03:21 +0530

    Create code.cpp

---
+++ DSA/Stack/Next Greater Element/code.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+
+    /* Function to find the next greater 
+    element for each element in the array */
+    vector<int> nextLargerElement(vector<int> arr) {
+        
+        int n = arr.size(); //size of array
+        
+        // To store the next greater elements
+        vector<int> ans(n);
+        
+        // Stack to get elements in LIFO fashion
+        stack<int> st;
+        
+        // Start traversing from the back
+        for(int i=n-1; i >= 0; i--) {
+            
+            // Get the current element
+            int currEle = arr[i];
+            
+            /* Pop the elements in the stack until 
+            the stack is not empty and the top 
+            element is not the greater element */
+            while(!st.empty() && st.top() <= currEle) {
+                st.pop();
+            }
+            
+            /* If the greater element is not 
+            found, stack will be empty */
+            if(st.empty()) 
+                ans[i] = -1;
+                
+            // Else store the answer
+            else 
+                ans[i] = st.top();
+            
+            /* Push the current element in the stack 
+            maintaining the decreasing order */
+            st.push(currEle);
+        }
+        
+        // Return the result
+        return ans;
+    }
+};
